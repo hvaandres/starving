8cfff1e6f607c9222a27e6d86b499bfe7676cea1
Andres Haro <hello@aharo.dev>
2025-01-24 17:15:58 -0700

    Initial Commit

---
+++ starving/starvingApp.swift
@@ -0,0 +1,17 @@
+//
+//  starvingApp.swift
+//  starving
+//
+//  Created by Alan Haro on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct starvingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
