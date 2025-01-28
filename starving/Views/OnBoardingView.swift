e69e620c0395921bae71efaa3a6980d688188177
Andres Haro <hello@aharo.dev>
2025-01-27 22:26:25 -0700

    Adding new file

--- starving/Views/OnBoardingView.swift
+++ starving/Views/OnBoardingView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct OnBoardingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/) // This is a placeholder
     }
 }
 
