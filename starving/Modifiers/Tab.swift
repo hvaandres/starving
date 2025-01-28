95fa0c070bea60eea5d9a0096d6969152bb40f7d
Andres Haro <hello@aharo.dev>
2025-01-28 11:09:05 -0700

    Adding my today's view, and making sure everything is working as expected

---
+++ starving/Modifiers/Tab.swift
@@ -0,0 +1,15 @@
+//
+//  Tab.swift
+//  starving
+//
+//  Created by Alan Haro on 1/28/25.
+//
+
+import Foundation
+
+enum Tab: Hashable {
+    case today
+    case items
+    case reminders
+    case settings
+}
